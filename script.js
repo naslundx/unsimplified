17c5dc99ac6a1ef17d5c44a48259e7301c569cd3
Marcus Näslund <naslundx@gmail.com>
2022-04-29 11:13:05 +0200

    settings, links, minor mobile adjustment

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 // DOM elements
 const datain = document.querySelector("input");
 const dataout = document.querySelector("#result");
+const refreshbtn = document.querySelector("#refresh");
+const pref_multiple = document.querySelector("#multiterm");
+const pref_complex = document.querySelector("#complex");
 
 // Helper functions
 const cartesian = (...a) => a.reduce((a, b) => a.flatMap(d => b.map(e => [d, e].flat()))).filter(x => x[0] != x[2]);
 const randint = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 // Inverse functions
-const basel_inv = (n) => {
+const basel_inv = (n, prefs) => {
+    if (!prefs.complex) {
+        return "";
+    }
+
     return "\\frac{" + (n * 6) + "}{\\pi^2}\\sum_{k=1}^{\\infty}\\frac{1}{k^2}";
 }
 
-// TODO add fun trig identities
+const two_three_inv = (n, prefs) => {
+    if (!prefs.complex) {
+        return "";
+    }
 
-const two_three_inv = (n) => {
     if (n >= 9 && n % 3 == 0) {
         const nthird = n / 3;
         return "\\sum_{k=1}^{\\infty}\\frac{" + nthird + "\\cdot k^2}{2^{k+1}}"
@@ -27,7 +36,7 @@ const two_three_inv = (n) => {
     return "";
 }
 
-const factorial_inv = (n) => {
+const factorial_inv = (n, prefs) => {
     let product = 1;
     let i = 1;
         
@@ -41,8 +50,12 @@ const factorial_inv = (n) => {
     return "";
 }
 
-const half_inv = (n) => {
-    if (n % 2 == 0) {
+const half_inv = (n, prefs) => {
+    if (!prefs.complex) {
+        return "";
+    }
+
+    if (n % 2 == 0 && n > 2) {
         let nhalf = n / 2;
         return "\\lim_{a->0^+}\\int_a^1\\frac{" + nhalf + "dx}{\\sqrt{x}}";
     }
@@ -50,7 +63,7 @@ const half_inv = (n) => {
     return "";
 }
 
-const power_two_inv = (n) => {
+const power_two_inv = (n, prefs) => {
     if (n && !(n & (n - 1))) {
         let nlog = Math.log2(n);
         return "2^{" + nlog + "}";
@@ -59,11 +72,19 @@ const power_two_inv = (n) => {
     return "";
 }
 
-const euler_inv = (n) => {
+const euler_inv = (n, prefs) => {
+    if (!prefs.complex) {
+        return "";
+    }
+
     return "\\frac{" + n + "e^{\\pi \\cdot i}}{i^2}";
 }
 
-const tanprod_inv = (n) => {
+const tanprod_inv = (n, prefs) => {
+    if (!prefs.complex) {
+        return "";
+    }
+
     if (n % 2 == 0) {
         return "";
     }
@@ -73,6 +94,10 @@ const tanprod_inv = (n) => {
     return "\\left( \\prod_{k=1}^{" + m + "} \\frac{k \\pi}{2 \\cdot" + m + " + 1} \\right)^2";
 }
 
+const simpledouble_inv = (n, prefs) => {
+    return "\\frac{" + (n * 2) + "}{2}"
+}
+
 const all_inv_functions = [
     basel_inv,
     two_three_inv,
@@ -80,33 +105,40 @@ const all_inv_functions = [
     half_inv,
     power_two_inv,
     euler_inv,
-    tanprod_inv
+    tanprod_inv,
+    simpledouble_inv,
 ];
 
 // Computation functions
-const _compute = (n) => {
-    let results = all_inv_functions.map(x => x(n)).filter(x => x);
+const _compute = (n, prefs) => {
+    let results = all_inv_functions.map(x => x(n, prefs)).filter(x => x);
     return results[randint(0, results.length)];
 }
 
-const compute = (number, parts) => {
-    parts = randint(1, parts + 1);
+const compute = (number, parts, prefs) => {
+    if (prefs.multiterm === false || number < 3) {
+        parts = 1;
+    } else {
+        parts = randint(1, parts + 1);
+    }
 
     subresults = [];
 
     while (parts-- > 1) {
-        let term = randint(1, number / parts);
-        subresults.push(_compute(term));
+        let term = randint(2, number / parts);
+        subresults.push(_compute(term, prefs));
         number -= term;
         console.log(subresults);
     }
 
-    subresults.push(_compute(number));
+    subresults.push(_compute(number, prefs));
     return subresults.join(" + ");
 }
 
 // UI
 const update = () => {
+    refreshbtn.classList.remove("visible");
+
     if (datain.value.length == 0) {
         dataout.innerText = "";
         return;
@@ -118,7 +150,15 @@ const update = () => {
         dataout.innerText = "Bara positiva heltal (än så länge)";
     }
 
-    const result = compute(number, 3);
+    let prefs = {
+        complex: complex.checked,
+        multiterm: multiterm.checked
+    }
+
+    console.log(prefs);
+
+    const result = compute(number, 3, prefs);
     dataout.innerText = `$$${result}$$`;
+    refreshbtn.classList.add("visible");
     MathJax.typeset();
 }
